2d3b1e807e03dc7f19e0faa9f0686d0b500056e3
donlatud <donlatud@gmail.com>
2026-01-08 12:16:23 +0700

    feat: implement email validation functionality with user feedback

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,43 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setEmail(e.target.value);
+  };
+
+  const handleValidate = (e) => {
+    e.preventDefault();
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (emailRegex.test(email)) {
+      setMessage(`✅ ${email}`);
+      setIsError(false);
+    } else {
+      setMessage(`❌ This ${email} is not valid please try again`);
+      setIsError(true);
+    }
+    setEmail("");
+  };
+
   return (
     <div className="App">
       <h1>Email Validation App</h1>
       <form className="email-container">
-        <h2 style={{ color: "red" }}>ผลลัพธ์จะมาแทนที่ตรงนี้</h2>
-        <input type="text" placeholder="Enter your email" />
+        <h2 style={{ color: isError ? "red" : "green" }}>{message}</h2>
+        <input
+          type="text"
+          placeholder="Enter your email"
+          value={email}
+          onChange={handleInputChange}
+        />
         <div className="buttons">
-          <button type="submit">Validate</button>
+          <button type="submit" onClick={handleValidate}>
+            Validate
+          </button>
         </div>
       </form>
     </div>
