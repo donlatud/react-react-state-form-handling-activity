2dce3e9ea866770d99751de69137d246ea4b0c82
donlatud <donlatud@gmail.com>
2026-01-08 10:53:12 +0700

    feat: implement input handling and display greeting message

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setInputValue(e.target.value);
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    setMessage(inputValue);
+    setInputValue("");
+  };
+
   return (
     <div className="App">
       <h1>Simple Greeting Message App 2</h1>
       <div className="greeting-container">
-        <h2>ผลลัพธ์จะมาแทนที่ข้อความนี้</h2>
-        <input type="text" />
+        <h2>{message}</h2>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
         <div className="buttons">
-          <button>Add</button>
+          <button onClick={handleAdd}>Add</button>
         </div>
       </div>
     </div>
